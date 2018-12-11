e83d493acaee3a80973b2798ca2f993cb1e2f7bf
Ron Craig <ron.craig@comcast.net>
2018-12-11 10:43:17 -0800

    add undefined steps in step_definitions file

---
+++ features/step_definitions/stepdefs.rb
@@ -0,0 +1,155 @@
+Given('a CSV file {string}input.csv{string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I ask whether the file is input.csv') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be told {string}Correct file{string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('a CSV file {string}anything else!{string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be told {string}Not the correct file, looking for input.csv{string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('the correct filename') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I ask if to read the filename') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be told the {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('a words array') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('ignoring the last words array {string}Portland{string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I ask to verify each words array {string}Portland{string} is a word') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be told {string}Portland{string} is {string}a word{string}') do |string, string2, string3, string4|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('ignoring the last words array {string}Stockholm{string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I ask to verify each words array {string}Stockholm{string} is a word') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be told {string}Stockholm{string} is {string}a word{string}') do |string, string2, string3, string4|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('ignoring the last words array {string}Port land{string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I ask to verify each words array {string}Port land{string} is a word') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be told {string}Port land{string} is {string}not a word{string}') do |string, string2, string3, string4|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('ignoring the last words array {string}St{int}ckh{int}lm{string}') do |string, int, int2, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I ask to verify each words array {string}St{int}ckh{int}lm{string} is a word') do |string, int, int2, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be told {string}St{int}ckh{int}lm{string} is {string}not a word{string}') do |string, int, int2, string2, string3, string4|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('ignoring the last words array {string}
+{string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I ask to verify each words array {string}
+{string} is a word') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be told {string}
+{string} is {string}not a word{string}') do |string, string2, string3, string4|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('the words array') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I ask if to verify the words array {string}
+{string} is a new line character') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be told {string}new line character is present{string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I ask if to verify the words array {string}n{string} is a new line character') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be told {string}new line character is not present{string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I ask if to verify the words array {string}anything else!{string} is a new line character') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('the array of words') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('ignoring the last words array {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I ask to sort the array of words in descending alphabetical order') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be told {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I ask to write a new file') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('the word array are stored as a single string') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('there should be a comma between each word') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('there should be a new line character at the end of the string') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
