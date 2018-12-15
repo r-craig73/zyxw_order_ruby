4b81e09df9db8d895cb384f1ff0c0b70d771abb8
Ron Craig <ron.craig@comcast.net>
2018-12-15 01:46:31 -0800

    add 2nd scenario steps, 6 out of 8 passing

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,12 +1,10 @@
 # Reverse City(ies) order
 
 Given('a CSV file {string}') do |given_filename|
-  # pending # Write code here that turns the phrase above into concrete actions
   @input_filename = given_filename
 end
 
 When('I ask the file is input.csv') do
-  # pending # Write code here that turns the phrase above into concrete actions
   def right_file_name(name)
     if name == 'input.csv'
       'Correct file'
@@ -18,6 +16,13 @@ When('I ask the file is input.csv') do
 end
 
 Then('I should be told {string}') do |expected_answer|
-  # pending # Write code here that turns the phrase above into concrete actions
   expect(@file_reply).to eq(expected_answer)
+end
+
+When("reading the correct filename") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be told the {string}') do |expected_array_answer|
+  pending # Write code here that turns the phrase above into concrete actions
 end
