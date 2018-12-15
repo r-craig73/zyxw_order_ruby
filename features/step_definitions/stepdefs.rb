af3c7246dbe11af735bf22b96a3fe76dc7358b1b
Ron Craig <ron.craig@comcast.net>
2018-12-15 00:11:25 -0800

    passing first Scenario Outline, 6 out 6 steps passing

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,13 +1,23 @@
 # Reverse City(ies) order
 
-Given('a CSV file {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Given('a CSV file {string}') do |given_filename|
+  # pending # Write code here that turns the phrase above into concrete actions
+  @input_filename = given_filename
 end
 
 When('I ask the file is input.csv') do
-  pending # Write code here that turns the phrase above into concrete actions
+  # pending # Write code here that turns the phrase above into concrete actions
+  def right_file_name(name)
+    if name == 'input.csv'
+      'Correct file'
+    else
+      'Not the correct file, looking for input.csv'
+    end
+  end
+  @file_reply = right_file_name(@input_filename)
 end
 
-Then('I should be told {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Then('I should be told {string}') do |expected_answer|
+  # pending # Write code here that turns the phrase above into concrete actions
+  expect(@file_reply).to eq(expected_answer)
 end
