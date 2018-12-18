74ff4ecda7383c85cafa16f379f00961a6d2ee7e
Ron Craig <ron.craig@comcast.net>
2018-12-17 22:49:25 -0800

    change filename 'input' string search using File.basename class

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,22 +1,23 @@
 # Reverse City(ies) order
 
 Given('a CSV file {string}') do |given_filename|
-  @input_filename = given_filename
+  @cities_filename = given_filename
 end
 
 When('I ask the file is input.csv') do
   def right_file_name(name)
-    if name == 'input.csv'
+    input_name = name.downcase
+    if File.basename(input_name, '.csv') == 'input'
       'Correct file'
     else
       'Not the correct file, looking for input.csv'
     end
   end
-  @file_reply = right_file_name(@input_filename)
+  @filename_reply = right_file_name(@cities_filename)
 end
 
 Then('I should be told {string}') do |expected_answer|
-  expect(@file_reply).to eq(expected_answer)
+  expect(@filename_reply).to eq(expected_answer)
 end
 
 When('reading the correct filename') do
