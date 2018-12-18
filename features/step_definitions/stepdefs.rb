2b83debdf625161216e66ca2b145f0430c50cd64
Ron Craig <ron.craig@comcast.net>
2018-12-18 02:37:33 -0800

    passing 3rd scenario steps, 9/9 steps passing

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -20,19 +20,18 @@ Then('I should be told {string}') do |expected_answer|
   expect(@filename_reply).to eq(expected_answer)
 end
 
-When('reading the correct filename') do
-  words = %w[Rome Cario Portland]
-  cities = words.is_a?(Array)
-  def an_array(name)
-    if name == true
-      'cities are stored in an array'
-    else
-      'oh no'
-    end
-  end
-  @array_reply = an_array(cities)
+When('I read the input filename') do
+  @cities = 'Rome,Cario,Portland'
 end
 
-Then('I should be told the {string}') do |expected_array_answer|
+When('insert the cities into an array') do
+  def an_array(name)
+    list_of_cities = name.split(',')
+    'cities are stored in an array' if list_of_cities.is_a?(Array) == true
+  end
+  @array_reply = an_array(@cities)
+end
+
+Then('I should see {string}') do |expected_array_answer|
   expect(@array_reply).to eq(expected_array_answer)
 end
