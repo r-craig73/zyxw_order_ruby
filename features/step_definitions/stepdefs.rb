ac451b476366055b93088f8030c215eee0bf0165
Ron Craig <ron.craig@comcast.net>
2018-12-20 00:46:07 -0800

    failed 14th scenario, 38/41 steps passing

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -86,3 +86,15 @@ end
 Then('there should be a long string of cities') do
   expect(@long_string).to match('Rome,Portland,Canton')
 end
+
+Given('a long string of cities') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I ask to write the long string of cities to filename output.csv') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('the new file should be created') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
